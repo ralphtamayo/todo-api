c99d22129e003694390c8a223f84089b5dd8ba77
unknown <tamayoralph24@gmail.com>
2019-11-13 21:55:54 +0800

    Add eslint and tsfmt for checking and formatting of files

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/*', async (ctx) => {
-    ctx.body = 'Hello World!';
+	ctx.body = 'Hello World!';
 });
 
 app.use(router.routes());
